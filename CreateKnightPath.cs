705891ba11817ee29e2e9c932c10e8ccdae2e45f
Dillon Welch <daw0328@gmail.com>
2024-07-10 19:58:27 -0400

    Starting a new project from scratch to try and get around weird issues

---
+++ CreateKnightPath.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace KnightPath
+{
+    public class CreateKnightPath
+    {
+        private readonly ILogger<CreateKnightPath> _logger;
+
+        public CreateKnightPath(ILogger<CreateKnightPath> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("CreateKnightPath")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Admin, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
