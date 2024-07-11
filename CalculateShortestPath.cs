959a6787f52b7657d08b2ea125fb631b3be31022
Dillon Welch <daw0328@gmail.com>
2024-07-10 21:04:27 -0400

    Add queue trigger function

---
+++ CalculateShortestPath.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Text.Json;
+using Azure.Storage.Queues.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace KnightPath
+{
+    public class CalculateShortestPath
+    {
+        private readonly ILogger<CalculateShortestPath> _logger;
+
+        public CalculateShortestPath(ILogger<CalculateShortestPath> logger)
+        {
+            _logger = logger;
+        }
+
+        // TODO: Move to Common
+        public class CreateKnightPathQueueMessage
+        {
+            public required string TrackingId { get; set; }
+            public required string Source { get; set; }
+            public required string Target { get; set; }
+        }
+
+        [Function(nameof(CalculateShortestPath))]
+        public void Run([QueueTrigger("knightpathqueue")] QueueMessage message)
+        {
+            _logger.LogInformation($"C# Queue trigger function processed: {message.MessageText}");
+
+            var input = JsonSerializer.Deserialize<CreateKnightPathQueueMessage>(message.MessageText);
+
+            _logger.LogInformation("Source {Source} and Target {Target} and Tracking ID {TrackingId}", input.Source, input.Target, input.TrackingId);
+        }
+    }
+}
