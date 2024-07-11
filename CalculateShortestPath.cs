1a13ebc356675bdc74bd2b1dbddce8a9c6546cf0
Dillon Welch <daw0328@gmail.com>
2024-07-10 21:32:53 -0400

    Add SQL Output binding that somehow works

--- CalculateShortestPath.cs
+++ CalculateShortestPath.cs
@@ -2,6 +2,7 @@ using System;
 using System.Text.Json;
 using Azure.Storage.Queues.Models;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.Sql;
 using Microsoft.Extensions.Logging;
 
 namespace KnightPath
@@ -24,18 +25,35 @@ namespace KnightPath
         }
 
         [Function(nameof(CalculateShortestPath))]
-        public void Run([QueueTrigger("knightpathqueue")] QueueMessage message)
+        [SqlOutput("dbo.Paths", connectionStringSetting: "SqlConnectionString")]
+        public Path Run([QueueTrigger("knightpathqueue")] QueueMessage message)
         {
             _logger.LogInformation($"C# Queue trigger function processed: {message.MessageText}");
 
             var input = JsonSerializer.Deserialize<CreateKnightPathQueueMessage>(message.MessageText);
 
-            _logger.LogInformation("Source {Source} and Target {Target} and Tracking ID {TrackingId}", input.Source, input.Target, input.TrackingId);
+            // _logger.LogInformation("Source {Source} and Target {Target} and Tracking ID {TrackingId}", input.Source, input.Target, input.TrackingId);
 
             var shortestPath = ShortestPathCalculator.CalculateShortestPath(input.Source, input.Target);
             var stringPath = String.Join(":", shortestPath);
 
+            var rawTrackingId = input.TrackingId;
+            if (!Guid.TryParse(rawTrackingId, out Guid trackingId))
+            {
+                // TODO: How to handle
+                _logger.LogError($"Invalid id format: {rawTrackingId}");
+                // return new BadRequestObjectResult("Invalid id format.");
+            }
+
             _logger.LogInformation("Shortest path is '{ShortestPath}'", stringPath);
+
+            return new Path() {
+                SourcePosition = input.Source,
+                TargetPosition = input.Target,
+                TrackingId = trackingId,
+                NumberOfMoves = shortestPath.Count - 1,
+                ShortestPath = stringPath
+            };
         }
     }
 }
---
+++ KnightPath.cs
@@ -0,0 +1,16 @@
+namespace KnightPath
+{
+  // TODO: How to set this up in the DB
+  // [Index(nameof(TrackingId), IsUnique = true)]
+  public class Path
+  {
+      // TODO: Comments
+      public int PathId { get; set; }
+      public required Guid TrackingId { get; set; }
+      public required string ShortestPath { get; set; }
+      public required int NumberOfMoves { get; set; }
+      // TODO: validate inputs
+      public required string SourcePosition { get; set; }
+      public required string TargetPosition { get; set; }
+  }
+}
