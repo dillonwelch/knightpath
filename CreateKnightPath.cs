4e8be1dbb409505bd24d98f0d8b043377e66ce8f
Dillon Welch <daw0328@gmail.com>
2024-07-10 20:53:07 -0400

    Remove usings

--- CreateKnightPath.cs
+++ CreateKnightPath.cs
@@ -1,7 +1,5 @@
 using System.Net;
 using System.Text.Json;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
