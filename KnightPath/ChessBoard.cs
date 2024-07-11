f38828c97a925156242f57bb58aae53e27c039db
Dillon Welch <daw0328@gmail.com>
2024-07-11 18:04:42 -0400

    Clean up error msg

--- KnightPath/ChessBoard.cs
+++ KnightPath/ChessBoard.cs
@@ -11,13 +11,13 @@ namespace KnightPath
             if (!Char.IsBetween(position[0], 'A', 'H'))
             {
               string positionX = position[0].ToString();
-              throw new ArgumentException("Invalid row identifier '" + positionX + "' in position '" + position + "'.");
+              throw new ArgumentException($"Invalid row identifier '{positionX}' in position '{position}'.");
             }
 
             int positionY = (int)(Char.GetNumericValue(position[1]) - 1);
             if (positionY < 0 || positionY >= Rows) 
             {
-              throw new ArgumentException("Invalid column identifier '" + (positionY + 1) + "' in position '" + position + "'.");
+              throw new ArgumentException($"Invalid column identifier '{positionY + 1}' in position '{position}'.");
             }
         }
     }
