60f3d74578f53f65fb5abda7ac740e34442475b7
Dillon Welch <daw0328@gmail.com>
2024-07-10 20:31:34 -0400

    This seems to queue and return http :)

--- CreateKnightPath.cs
+++ CreateKnightPath.cs
@@ -1,7 +1,9 @@
+using System.Net;
 using System.Text.Json;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
 namespace KnightPath
@@ -21,20 +23,30 @@ namespace KnightPath
             public string? Target { get; set; }
         }
 
+        public class MultiResponse
+        {
+            [QueueOutput("knightpathqueue")]
+            public string? Message { get; set; }
+            public required HttpResponseData HttpResponse { get; set; }
+        }
+
         [Function("CreateKnightPath")]
-        [QueueOutput("knightpathqueue")]    
-        public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
+        public async Task<MultiResponse> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             var input = JsonSerializer.Deserialize<CreateKnightPathRequest>(requestBody);
             var trackingId = Guid.NewGuid().ToString();
 
-            return new OkObjectResult(new {
-                TrackingId = trackingId,
-                Source = input.Source,
-                Target = input.Target
-            });
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            await response.WriteStringAsync(trackingId);
+
+            return new MultiResponse() 
+            {
+                Message = trackingId,
+                HttpResponse = response
+            };
         }
     }
 }
